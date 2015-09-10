09f59e28a73b515a084de51328bedc8cfdaa097b
hardik thaker <hardik.thaker1994@yahoo.com>
2015-09-11 00:50:17 +0530

    added a member register class

--- SksTimeTable/DataAccessLayer.cs
+++ SksTimeTable/DataAccessLayer.cs
@@ -53,6 +53,34 @@ namespace SksTimeTable
                 return dSet;
             }
         }
+        public static bool isMemRegSuccessful(string username, string encPass)
+        {
+            using (MySqlConnection connection = ConnectionManager.GetDatabaseConnection())
+            {
+                try
+                {
+                    MySqlCommand command = new MySqlCommand("sp_regMembers", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    //command.Parameters.Add("@username", MySqlDbType.VarChar).Value = username;
+                    //command.Parameters.Add("@enyPass", MySqlDbType.VarChar).Value = encPass;
+
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@encPass", encPass);
+
+                    //MySqlDataAdapter adapter = new MySqlDataAdapter();
+                    //adapter.InsertCommand = command;
+                    command.ExecuteNonQuery();
+                    connection.Close();
+    
+                    return true;
+                }
+                catch (Exception) {
+                    return false;
+                }
+
+                
+            }
+        }
 
     }
 }
