57450e8c8128ec01e4a93d937dd412b5297dc88f
hardik thaker <hardik.thaker1994@yahoo.com>
2015-09-12 00:29:46 +0530

    initial commite

--- SksTimeTable/default.aspx.cs
+++ SksTimeTable/default.aspx.cs
@@ -18,7 +18,8 @@ namespace SksTimeTable
         protected void loginSubmitBtn_Click(object sender, EventArgs e)
         {
             string username = UserNameTB.Text;
-            PwdEncrypt.passwordEnc(PasswordTB.Text);
+            //PwdEncrypt.passwordEnc(PasswordTB.Text);
+            string password = PasswordTB.Text;
             try
             {
                 DataSet dSet = DataAccessLayer.isMemberExits(username, password);
@@ -36,48 +37,6 @@ namespace SksTimeTable
             catch(Exception ex) {
                 Response.Write(ex.Message);
             }
-
-
-
-
-
-
-
-
-                ////connectionString
-                //string conStr = ConfigurationManager.ConnectionStrings["mySQLConnString"].ConnectionString;
-                //try
-                //{
-                //    //this type of connection can prevent SQL Injection
-                //    using (MySqlConnection con = new MySqlConnection(conStr))
-                //    {
-                //        con.Open();//open the SQL connection
-                //        //this type of connection can prevent SQL Injection
-                //        using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM users WHERE users='" + UserNameTB.Text + "' and password='" + PasswordTB.Text + "'", con))
-                //        {
-
-                //            //int sdr = Convert.ToInt32(cmd.ExecuteScalar().ToString());
-                //            MySqlDataReader sdr = cmd.ExecuteReader();
-                //            // msg.Text = sdr.ToString();
-
-                //            if (sdr.Read())
-                //            {
-                //                sdr.Close();
-                //                con.Close();
-                //                Session["username"] = UserNameTB.Text;
-                //                Response.Redirect("Admin.aspx");
-                //            }
-                //            else
-                //            {
-                //                msg.Text = "Wrong user id or Password";
-                //            }
-                //        }
-                //    }
-                //}
-                //catch (Exception ex)
-                //{
-                //    Response.Write(ex.Message);
-                //}
         }
     }
 }
